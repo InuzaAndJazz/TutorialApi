db3f6d53698cca0fabf04f1061ac23c38f8cc46a
RIGSBY\darkm <angeleon1987@gmail.com>
2022-07-06 23:32:52 -0400

    feature/changePassword

--- src/main/java/cl/developteam/controller/UserController.java
+++ src/main/java/cl/developteam/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,4 +38,20 @@ public class UserController {
 			return "ERROR: El usuario no se encuentra registrado";
 		}
 	}
+	
+	@PatchMapping("/user/changePassword")
+	@ResponseBody
+	public String cambioPassword(@RequestParam String email, @RequestParam String password, @RequestParam String newPassword) {
+		if (usuarios.containsKey(email)) {
+			if (usuarios.get(email).equals(password)) {
+				usuarios.replace(email, password, newPassword);
+				return "OK: Contraseña cambiada con éxito.";
+			} else {
+				return "ERROR: Usuario o contraseña incorrectos.";
+			}
+		} else {
+			return "ERROR: Usuario no existente.";
+		}
+	}
+
 }
